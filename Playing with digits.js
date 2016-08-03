218078b163bc58921203932af1898fa25de3719a
Chris Wilson <chris2097@gmail.com>
2016-08-03 11:04:14 +0100

    Completed playing with digits

--- Playing with digits.js
+++ Playing with digits.js
@@ -1,10 +1,18 @@
-function digPow(n, p){
-    var count = 0;
-    for (var i = 0; )
+function digPow(n, p) {
+    var arr = [],
+        k;
+    for (var i = 0; i < n.toString().split("").length; i++) {
+        arr.push(Math.pow(n.toString().split("")[i], p + i));
+    }
 
-    if (count = 0){
+    k = (arr.reduce(function (a, b) {
+        return a + b;
+    }) / n);
+    if (Number.isInteger(k)) {
+        return k;
+    } else {
         return -1;
-    }else{
-        return count;
     }
 }
+
+digPow(89, 1);
